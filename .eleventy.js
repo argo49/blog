a6958c796c4e4f9776f78f1570fdadde0bdf1e74
Tyler Argo <targo@yelp.com>
2020-05-22 10:30:19 -0700

    Added PWA ability and icons

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,23 @@
 const CleanCSS = require("clean-css");
+const pluginPWA = require("eleventy-plugin-pwa");
 
-module.exports = function(config) {
-  config.addCollection("posts", collection => {
+module.exports = function (config) {
+  config.addCollection("posts", (collection) => {
     return collection
       .getFilteredByGlob("./posts/*.md")
-      .filter(post => {
+      .filter((post) => {
         return post.data.status != "draft";
       })
       .reverse();
   });
 
-  config.addFilter("cssmin", function(code) {
+  config.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  config.addPlugin(pluginPWA);
+
+  config.addPassthroughCopy("favicon.ico");
+  config.addPassthroughCopy("manifest.json");
+  config.addPassthroughCopy("images");
 };
