96cc27b60d268eafbc925eeb1e776a6ccc2bf744
Tyler Argo <targo@yelp.com>
2021-07-22 19:54:04 -0700

    Fixed build error when removing the eleventy plugin pwa

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 const CleanCSS = require("clean-css");
-const pluginPWA = require("eleventy-plugin-pwa");
 const fs = require("fs");
 
 module.exports = function (config) {
@@ -15,8 +14,6 @@ module.exports = function (config) {
   config.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
-
-  config.addPlugin(pluginPWA);
 
   config.setBrowserSyncConfig({
     callbacks: {
